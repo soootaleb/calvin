57887f1b2797999dcd5ae7f8ec77d58c32b8f7b5
Sofiane Taleb <sofiane.taleb.94@gmail.com>
2017-11-16 12:16:43 +0100

    Added data

--- tf_tuto_mnist_deep.py
+++ tf_tuto_mnist_deep.py
@@ -1,7 +1,7 @@
 import tensorflow as tf
 from tensorflow.examples.tutorials.mnist import input_data
 
-mnist = input_data.read_data_sets('data', one_hot=True)
+mnist = input_data.read_data_sets('data/tfmnist', one_hot=True)
 
 
 def weight_variable(shape):
